4cd1de78021f4a632cb4cf33a19b7259dfe0f748
galeNight <97892095+galeNight@users.noreply.github.com>
2023-01-16 18:10:56 +0100

    Add files via upload

---
+++ opgave 4/introuge opgaver/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace introuge_opgaver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("fornavn: jannick");
+            Console.WriteLine("efternavn: mørup");
+            Console.WriteLine("addresse: spruvegården 26 st,tv");
+            Console.WriteLine("postnummer: 2650");
+            Console.WriteLine("by: hvidovre");
+            Console.WriteLine("mail: jannickmorup@gmail.com eller jann854j@elev.tec.dk");
+            Console.WriteLine("mobilnummer: 61999868");
+            Console.ReadKey();
+        }
+    }
+}
